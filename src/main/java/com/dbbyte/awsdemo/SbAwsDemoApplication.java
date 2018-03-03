c4440d0b273596b33e3b3617dd3728a10f13aa2c
adriancuria <curia.gheorghe@yahoo.com>
2018-03-03 13:14:10 +0200

    first

---
+++ src/main/java/com/dbbyte/awsdemo/SbAwsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.dbbyte.awsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAwsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAwsDemoApplication.class, args);
+	}
+}
